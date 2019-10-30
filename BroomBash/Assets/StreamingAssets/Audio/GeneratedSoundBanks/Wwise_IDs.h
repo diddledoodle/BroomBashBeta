f919b8a55a6c8edb1f18af6672ac16c214c97d78
jamespost <32137126+jamespost@users.noreply.github.com>
2019-10-30 09:07:41 -0500

    implemented ALL SFX in the Wwise project
    all sounds are hooked up in Wwise, just need to hook them up in-game now

--- BroomBash/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ BroomBash/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,12 +13,47 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_BB_SX_GAME_AMB_CROWD_LARGE = 2811784222U;
+        static const AkUniqueID PLAY_BB_SX_GAME_AMB_CROWD_SMALL = 374084730U;
+        static const AkUniqueID PLAY_BB_SX_GAME_AMB_MEOWING = 3417746653U;
+        static const AkUniqueID PLAY_BB_SX_GAME_AMB_SEAGULLS = 2968948371U;
+        static const AkUniqueID PLAY_BB_SX_GAME_AMB_WATER_LAP_STONE = 758436608U;
+        static const AkUniqueID PLAY_BB_SX_GAME_AMB_WATER_WAVES = 260817731U;
+        static const AkUniqueID PLAY_BB_SX_GAME_INT_DELIVERY_DROPOFF = 1048965709U;
+        static const AkUniqueID PLAY_BB_SX_GAME_INT_DELIVERY_PICKUP = 628949193U;
+        static const AkUniqueID PLAY_BB_SX_GAME_INT_ENTER_ACTIVE_PICKUP = 3783710516U;
+        static const AkUniqueID PLAY_BB_SX_GAME_INT_EXIT_ACTIVE_PICKUP = 1170827374U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_BROOM_ACCELERATE = 291020428U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_BROOM_DECELERATE = 3485431345U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_BROOM_FLYING = 3676326078U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_BROOM_STOP = 2441865659U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_BROOM_TAKEOFF = 2242722565U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_IMPACT_BUILDING = 3843282546U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_IMPACT_GROUND = 2876800831U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_IMPACT_WATER = 3283685423U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_SKIM_GROUND = 3817973033U;
+        static const AkUniqueID PLAY_BB_SX_GAME_PLR_SKIM_WATER = 789946661U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_DELIVERY_ACCEPT = 1095623268U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_DELIVERY_DECLINE = 4291039282U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_DELIVERY_NEW = 1081189030U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_EXITGAME = 1359399331U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_TEXT_ADVANCE = 4043862979U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_TEXT_CLOSE = 1303049037U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_TIMER_LOW = 2394352179U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_TIMER_OUT = 2562275565U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_XP_GAINED = 3865043706U;
+        static const AkUniqueID PLAY_BB_SX_GAME_UI_XP_LOST = 214100990U;
+        static const AkUniqueID PLAY_BB_SX_MENU_UI_MAIN_NAVIGATION = 1875305114U;
         static const AkUniqueID PLAY_BB_SX_MENU_UI_MAIN_START = 661752896U;
     } // namespace EVENTS
 
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIENCE = 85412153U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID INTERACTIBLES = 2987141838U;
+        static const AkUniqueID PLAYER = 1069431850U;
         static const AkUniqueID UI = 1551306167U;
     } // namespace BANKS
 
