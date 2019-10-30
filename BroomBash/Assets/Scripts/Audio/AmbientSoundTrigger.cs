d3503b41998fdd0f52f9d95a3034f740f58e4be8
jamespost <32137126+jamespost@users.noreply.github.com>
2019-10-30 12:46:31 -0500

    added meowing ambient sound emitters nearby most pickup/dropoff locations

---
+++ BroomBash/Assets/Scripts/Audio/AmbientSoundTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientSoundTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //play the ambient sound at random intervals between 10 and 20 seconds
+        InvokeRepeating("PlayAmbientSound", Random.Range(10, 20), Random.Range(10, 20));
+    }
+
+
+    private void PlayAmbientSound()
+    {
+        //replace string with wwise event that you want to trigger at a random interval
+        AkSoundEngine.PostEvent("play_bb_sx_game_amb_meowing", gameObject);
+    }
+}
